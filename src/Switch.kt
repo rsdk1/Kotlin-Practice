48028fc7169636a4b97b875460dcae4745422555
rsdk1 <rs.dk.00001@gmail.com>
2025-05-04 19:24:04 +0530

    Switch case

---
+++ src/Switch.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    val day = 4
+
+    when (day)
+    {
+        1 -> println("Mon")
+        2 -> println("Tues")
+        3 -> println("Wed")
+        4 -> println("Thurs")
+        5 -> println("Fri")
+        6 -> println("Satur")
+        7 -> println("Sun")
+        else -> println("Invalid")
+    }
+}
